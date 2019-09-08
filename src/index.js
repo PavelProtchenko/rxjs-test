cf8c7e12cdea9fd73689a9b10b9a91b4e4516185
PavelProtchenko <protchenkopa@gmail.com>
2019-09-08 16:44:45 +0300

    fix empty request sending for github api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {fromEvent} from 'rxjs'
-import {map, debounceTime, distinctUntilChanged, switchMap, mergeMap} from 'rxjs/operators'
+import {EMPTY, fromEvent} from 'rxjs'
+import {map, debounceTime, distinctUntilChanged, switchMap, mergeMap, tap, catchError, filter} from 'rxjs/operators'
 import {ajax} from 'rxjs/ajax'
 
 const url = 'https://api.github.com/search/users?q='
@@ -12,7 +12,11 @@ const stream$ = fromEvent(search, 'input')
     map(e => e.target.value),
     debounceTime(1000),
     distinctUntilChanged(),
-    switchMap(v => ajax.getJSON(url + v)),
+    tap(() => result.innerHTML = ''),
+    filter(v => v.trim()),
+    switchMap(v => ajax.getJSON(url + v).pipe(
+      catchError(err => EMPTY)
+    )),
     map(response => response.items),
     mergeMap(items => items)
   )

fb0237ba53e19ad7c4f86bea1ebaf22209c8b929
PavelProtchenko <protchenkopa@gmail.com>
2019-09-08 15:43:25 +0300

    add ajax for new asynchronous stream and switchMap operator for switch between streams

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import {fromEvent} from 'rxjs'
-import {map, debounceTime, distinctUntilChanged} from 'rxjs/operators'
+import {map, debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators'
+import {ajax} from 'rxjs/ajax'
 
-const url = 'https://api.github.com/search/users/q='
+const url = 'https://api.github.com/search/users?q='
 
 const search = document.getElementById('search')
 
@@ -9,7 +10,8 @@ const stream$ = fromEvent(search, 'input')
   .pipe(
     map(e => e.target.value),
     debounceTime(1000),
-    distinctUntilChanged()
+    distinctUntilChanged(),
+    switchMap(v => ajax.getJSON(url + v))
   )
 
 stream$.subscribe(value => {
